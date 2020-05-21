e53f812eb404d987a8246df76c7f101fcaa960c0
truptigaonkar <trups11.trupti@gmail.com>
2020-05-21 16:12:57 +0200

    Updates

---
+++ precache-manifest.763cc07ad01cefa457c973757dccaf63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d5bbc012d9a98812c52bd3810ce99e6",
+    "url": "/react-reused-components-usage/index.html"
+  },
+  {
+    "revision": "1448206500ab6784ea79",
+    "url": "/react-reused-components-usage/static/css/2.774e89e5.chunk.css"
+  },
+  {
+    "revision": "3917ecf778289c72da9a",
+    "url": "/react-reused-components-usage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1448206500ab6784ea79",
+    "url": "/react-reused-components-usage/static/js/2.20f8c121.chunk.js"
+  },
+  {
+    "revision": "3205f2f97be94286aaab611b41fbc37d",
+    "url": "/react-reused-components-usage/static/js/2.20f8c121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3917ecf778289c72da9a",
+    "url": "/react-reused-components-usage/static/js/main.0962f9c7.chunk.js"
+  },
+  {
+    "revision": "f9219d3e9c5dc4c1f35f",
+    "url": "/react-reused-components-usage/static/js/runtime-main.5f820119.js"
+  }
+]);
